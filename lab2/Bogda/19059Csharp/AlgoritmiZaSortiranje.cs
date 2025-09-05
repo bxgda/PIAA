53a429a09d862be469c3f962685afc02bda2b550
bxgda <jovan.bogdanovic@elfak.rs>
2025-09-05 18:02:55 +0200

    bogda

---
+++ lab2/Bogda/19059Csharp/AlgoritmiZaSortiranje.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _19059Csharp
+{
+    internal class AlgoritmiZaSortiranje
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        private static void StampajRezultate(TimeSpan vreme, long memorija)
+        {
+            Console.WriteLine($"- vreme: {vreme.TotalSeconds} s\n- memorija: {memorija} B");
+        }
+
+        public static void Selection(int[] niz)
+        {
+            Stopwatch stoperica = new Stopwatch();
+            long memorijaNaPocetku = GC.GetTotalMemory(false);
+
+            int n = niz.Length;
+
+            stoperica.Start();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+
+                for (int j = i; j < n; j++)
+                    if (niz[j] < niz[min])
+                        min = j;
+
+                Swap(ref niz[i], ref niz[min]);
+            }
+
+            stoperica.Stop();
+            TimeSpan vreme = stoperica.Elapsed;
+            long memorijaNaKraju = GC.GetTotalMemory(false);
+
+            Console.WriteLine("### Selection:");
+            StampajRezultate(vreme, memorijaNaKraju - memorijaNaPocetku);
+        }
+
+        public static void Quick(int[] niz)
+        {
+            Stopwatch stoperica = new Stopwatch();
+            long memorijaNaPocetku = GC.GetTotalMemory(false);
+            stoperica.Start();
+
+            QuickSort(niz, 0, niz.Length - 1);
+
+            stoperica.Stop();
+            TimeSpan vreme = stoperica.Elapsed;
+            long memorijaNaKraju = GC.GetTotalMemory(false);
+
+            Console.WriteLine("### Quick:");
+            StampajRezultate(vreme, memorijaNaKraju - memorijaNaPocetku);
+        }
+
+        private static void QuickSort(int[] niz, int pocetak, int kraj)
+        {
+            if (pocetak < kraj)
+            {
+                int pivot = Partition(niz, pocetak, kraj);
+                QuickSort(niz, pocetak, pivot - 1);
+                QuickSort(niz, pivot + 1, kraj);
+            }
+        }
+
+        private static int Partition(int[] niz, int pocetak, int kraj)
+        {
+            int pivot = niz[kraj];
+            int indeks = pocetak - 1;
+
+            for (int i = pocetak; i < kraj; i++) 
+            {
+                if (niz[i] < pivot)
+                {
+                    indeks++;
+                    if (indeks != i)                   
+                        Swap(ref niz[indeks], ref niz[i]);
+                }
+            }
+            if (indeks + 1 != kraj)                   
+                Swap(ref niz[indeks + 1], ref niz[kraj]);
+            return indeks + 1;
+        }
+
+        public static void Counting(int[] nizA)             //trebalo bi da se i prosledi opseg brojeva ali mi podrazumevamo da je 10 000
+        {
+            Stopwatch stoperica = new Stopwatch();
+            long memorijaNaPocetku = GC.GetTotalMemory(false);
+            stoperica.Start();
+
+            int[] nizB = new int[nizA.Length];
+            int[] nizC = new int[10001];
+
+            for (int i = 0; i < nizC.Length; i++)
+                nizC[i] = 0;
+
+            for (int i = 0; i < nizA.Length; i++)
+                nizC[nizA[i]]++;
+
+            for (int i = 1; i < nizC.Length; i++)
+                nizC[i] += nizC[i - 1];
+
+            for (int i = nizA.Length - 1; i >= 0; i--)
+            {
+                nizB[nizC[nizA[i]] - 1] = nizA[i];         
+                nizC[nizA[i]]--;
+            }
+            for (int i = 0; i < nizA.Length; i++)
+                nizA[i] = nizB[i];
+
+            stoperica.Stop();
+            TimeSpan vreme = stoperica.Elapsed;
+            long memorijaNaKraju = GC.GetTotalMemory(false);
+
+            Console.WriteLine("### Counting:");
+            StampajRezultate(vreme, memorijaNaKraju - memorijaNaPocetku);
+        }
+    }
+}
---
+++ lab2/Bogda/19059Csharp/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace _19059Csharp
+{
+    internal class Program
+    {
+        public static void StampajNiz(int[] niz)
+        {
+            for (int i = 0; i < niz.Length; i++)
+                Console.WriteLine(niz[i]);
+        }
+
+        public static void Sortiraj(int[] niz, string algoritam)
+        {
+            switch (algoritam)
+            {
+                case "Selection":
+                    AlgoritmiZaSortiranje.Selection(niz);
+                    Console.WriteLine($"- Min. cena je: {IzracunajMinCenu(niz)}\n");
+                    break;
+
+                case "Quick":
+                    AlgoritmiZaSortiranje.Quick(niz);
+                    Console.WriteLine($"- Min. cena je: {IzracunajMinCenu(niz)}\n");
+                    break;
+
+                case "Counting":
+                    AlgoritmiZaSortiranje.Counting(niz);
+                    Console.WriteLine($"- Min. cena je: {IzracunajMinCenu(niz)}\n");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public static int[] GenerisiNiz(int duzinaNiza)
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            int[] niz = new int[duzinaNiza];
+
+            for (int i = 0; i < duzinaNiza; i++)
+                niz[i] = rnd.Next(0, 10001);
+
+            return niz;
+        }
+
+        // resenje 2. problema
+        public static int IzracunajMinCenu(int[] niz)           //trebalo bi i k da se prosledi ali mi podrazumevamo da je 0.2
+        {
+            double k = niz.Length * 0.2, brojSlatkisa = 0;
+            int cena = 0, i = 0;
+
+            while (brojSlatkisa <= niz.Length)
+            {
+                cena += niz[i++];
+                brojSlatkisa += k;
+            }
+
+            return cena;
+        }
+
+        public static void PokreniSve()
+        {
+            int[] velicineNizova = { 100, 1000, 10000, 100000, 1000000, 10000000 };
+
+            for (int i = 0; i < velicineNizova.Length; i++)
+            {
+                Console.WriteLine($"*** Velicina niza: {velicineNizova[i]}\n");
+
+                int[] niz1 = GenerisiNiz(velicineNizova[i]);            
+                int[] niz2 = (int[])niz1.Clone();
+                int[] niz3 = (int[])niz1.Clone();
+
+                // ne pokrecem selection sort za nizove vece od 1 milion jer je presporo
+                if (i < 5)                          
+                    Sortiraj(niz1, "Selection");
+
+                Sortiraj(niz2, "Quick");
+
+                Sortiraj(niz3, "Counting");
+
+                Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            PokreniSve();
+        }
+    }
+}
